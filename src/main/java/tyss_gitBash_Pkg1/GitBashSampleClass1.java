45b8b41aae7eea07908f55f0af50b61df1f0434b
arvarier <103212028+arvarier@users.noreply.github.com>
2022-07-25 13:14:01 +0530

    Update GitBashSampleClass1.java

--- src/main/java/tyss_gitBash_Pkg1/GitBashSampleClass1.java
+++ src/main/java/tyss_gitBash_Pkg1/GitBashSampleClass1.java
@@ -4,6 +4,7 @@ public class GitBashSampleClass1 {
 
 	public static void main(String[] args) {
 		System.out.println("First sample class in git bash");
+		System.out.println("modified in github and pull from eclipse");
 
 	}
 
